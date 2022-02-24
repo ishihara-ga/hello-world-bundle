1aedb1c0e1dc64c4933ff03bc839c10ae2d98c1c
Miya Ishihara <miya.ishihara@grundig-akademie.de>
2022-02-24 14:35:26 +0100

    added controller

---
+++ src/Controller/TestController.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of Seminar Bundle.
+ *
+ * (c) Grundig Akademie
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace GrundigAkademie\HelloWorldBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Twig\Environment as TwigEnvironment;
+
+class NewsletterController extends AbstractController
+{
+    /**
+     * @Route("/newsletter",
+     *     name=TestController::class,
+     *     defaults={"_scope": "frontend"}
+     * )
+     */
+    public function __invoke(): Response
+    {
+        return new Response('Hello');
+    }
+
+
+
+}
